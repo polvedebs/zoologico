1e5c7a1e2a7986f171bbaec580c661121dd35f88
polvedeb <deborinha_bertoso@hotmail.com>
2022-10-06 23:26:57 -0300

    funcionou o slider

---
+++ site.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function () {
+    let slideIndex = 0;
+
+    showSlides();
+
+    function showSlides() {
+        let i;
+
+        let slides = document.querySelectorAll(".mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        console.log(slides[0])
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+    // code here
+});
